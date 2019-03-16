ab7f1aae6f6c0eb2b9605c5be3d03e9f18b8bb04
zeynepozturk <zeynep1404@gmail.com>
2019-03-17 01:50:22 +0300

    added model

---
+++ train.jl
@@ -0,0 +1,135 @@
+using Pkg; for p in ("Knet","PyCall","Plots"); haskey(Pkg.installed(),p) || Pkg.add(p); end
+using Base.Iterators: flatten, vcat
+using Statistics: mean
+using Knet: Knet, AutoGrad, param, param0, mat, RNN, relu, Data, adam, progress, nll, zeroone
+using PyCall
+push!( LOAD_PATH, "./" )
+include("utils.jl")
+include("model.jl")
+
+#using utils
+
+### NPZ did not work because while loading the files encoding must be specified
+### and NPZ does not have that capability
+#data=npzread("/Users/zeynepozturk/dersler/comp541/data/cat.npz")
+
+#@pyimport numpy as np
+np=pyimport("numpy")
+
+data_dir = "/Users/zeynepozturk/dersler/comp541/data/"
+
+model_params=get_default_hparams()
+
+function load_dataset(data_dir, model_params; inference_mode=false)
+    train_strokes = Float32[]; valid_strokes = Float32[]; test_strokes = Float32[]
+    #train_y = Float32[]; valid_y = Float32[]; test_y = Float32[]
+    datasets = model_params["data_set"]
+    #datasets = readlines("classes.txt")
+    #println(datasets)
+    for (idx, dataset) in enumerate(datasets)
+        #println("$idx, $dataset")
+        #global train_strokes, valid_strokes, test_strokes#, train_y, valid_y, test_y
+        fullpath = string(data_dir,dataset,".npz")
+        #println(fullpath)
+        data=np.load(fullpath, encoding="latin1")
+        #println(size(train_strokes))
+        if isempty(train_strokes)
+            train_strokes=convert(Array{Array{Float32}},get(data, PyObject, "train"))
+            #println(size(train_strokes))
+            valid_strokes=convert(Array{Array{Float32}},get(data, PyObject, "valid"))
+            test_strokes=convert(Array{Array{Float32}},get(data, PyObject, "test"))
+            #train_y = repeat([idx],size(train_strokes,1))
+            #valid_y = repeat([idx],size(valid_strokes,1))
+            #test_y = repeat([idx],size(test_strokes,1))
+        else
+            train_strokes=vcat(train_strokes,convert(Array{Array{Float32}},get(data, PyObject, "train")))
+            #println(size(train_strokes))
+            valid_strokes=vcat(valid_strokes,convert(Array{Array{Float32}},get(data, PyObject, "valid")))
+            test_strokes=vcat(test_strokes,convert(Array{Array{Float32}},get(data, PyObject, "test")))
+            #train_y = vcat(train_y,repeat([idx],size(train_strokes,1)))
+            #valid_y = vcat(valid_y,repeat([idx],size(valid_strokes,1)))
+            #test_y = vcat(test_y,repeat([idx],size(test_strokes,1)))
+        end
+    end
+
+    #println(size(train_strokes[1]))
+
+    all_strokes = vcat(train_strokes, valid_strokes, test_strokes)
+    num_points = 0
+
+    for stroke in all_strokes
+        #global num_points
+      num_points += size(stroke,1)
+    end
+    avg_len = num_points / size(all_strokes,1)
+    println("Dataset nb of instances $(size(all_strokes,1)) ($(size(train_strokes,1))
+    / $(size(valid_strokes,1)) / $(size(test_strokes,1))) with average length of strokes $avg_len")
+
+
+    # calculate the max strokes we need.
+    max_seq_len = get_max_len(all_strokes)
+    # overwrite the hps with this calculation.
+    model_params["max_seq_len"] = max_seq_len
+
+    println("Maximum sequence length is $max_seq_len")
+    #println("train max len: ", get_max_len(train_strokes) )
+
+    eval_model_params = copy_hparams(model_params)
+
+    eval_model_params["use_input_dropout"] = 0
+    eval_model_params["use_recurrent_dropout"] = 0
+    eval_model_params["use_output_dropout"] = 0
+    eval_model_params["is_training"] = 1
+
+    if inference_mode
+      eval_model_params["batch_size"] = 1
+      eval_model_params["is_training"] = 0
+    end
+
+    sample_model_params = copy_hparams(eval_model_params)
+    sample_model_params["batch_size"] = 1  # only sample one at a time
+    sample_model_params["max_seq_len"] = 1  # sample one point at a time
+
+    train_set = preprocess(DataLoader(
+        train_strokes;
+        batch_size=model_params["batch_size"],
+        max_seq_len=model_params["max_seq_len"],
+        random_scale_factor=model_params["random_scale_factor"],
+        augment_stroke_prob=model_params["augment_stroke_prob"]))
+
+    #println("train max len after: ", get_max_len(train_set.strokes) )
+    #println("train 1 max len after: ", size(train_set.strokes[1]) )
+    #println("train 7000 max len after: ", size(train_set.strokes[7000]) )
+
+    normalizing_scale_factor = calculate_normalizing_scale_factor(train_set)
+    #println(normalizing_scale_factor)
+
+    train_set = normalize(train_set, normalizing_scale_factor)
+
+    valid_set = preprocess(DataLoader(
+        valid_strokes;
+        batch_size=eval_model_params["batch_size"],
+        max_seq_len=eval_model_params["max_seq_len"],
+        random_scale_factor=0.0,
+        augment_stroke_prob=0.0))
+    valid_set = normalize(valid_set, normalizing_scale_factor)
+
+    test_set = preprocess(DataLoader(
+        test_strokes;
+        batch_size=eval_model_params["batch_size"],
+        max_seq_len=eval_model_params["max_seq_len"],
+        random_scale_factor=0.0,
+        augment_stroke_prob=0.0))
+    test_set = normalize(test_set, normalizing_scale_factor)
+
+    result = [
+        train_set, valid_set, test_set, model_params, eval_model_params,
+        sample_model_params
+    ]
+    return result
+end
+
+train_set, valid_set, test_set, model_params, eval_model_params,
+sample_model_params = load_dataset(data_dir, model_params; inference_mode=false)
+
+random_batch(train_set)
--- utils.jl
+++ utils.jl
@@ -1,4 +1,5 @@
-using Statistics
+using Statistics: std
+using Random
 
 function get_max_len(strokes)
   """Return the maximum length of an array of strokes."""
@@ -12,10 +13,41 @@ function get_max_len(strokes)
   return max_len
 end
 
+function augment_strokes(strokes, prob=0.0)
+  """Perform data augmentation by randomly dropping out strokes."""
+  # drop each point within a line segments with a probability of prob
+  # note that the logic in the loop prevents points at the ends to be dropped.
+  result = []
+  prev_stroke = [0, 0, 1]
+  count = 0
+  stroke = [0, 0, 1]  # Added to be safe.
+  for i in 1:size(strokes,1)
+      #println("iteration: $i")
+      #println("strokes size: ", size(strokes))
+    candidate = [strokes[i,1], strokes[i,2], strokes[i,3]]
+    if candidate[3] == 1 || prev_stroke[3] == 1
+      count = 0
+    else
+      count += 1
+    end
+    urnd = rand()  # uniform random variable
+    if candidate[3] == 0 && prev_stroke[3] == 0 && count > 2 && urnd < prob
+      stroke[1] += candidate[1]
+      stroke[2] += candidate[2]
+    else
+      stroke = candidate
+      prev_stroke = stroke
+      push!(result, stroke)
+    end
+  end
+  result = hcat(result...)'
+  return result
+end
+
 mutable struct DataLoader
     strokes
     batch_size
-    max_seq_length
+    max_seq_len
     scale_factor
     random_scale_factor
     augment_stroke_prob
@@ -24,13 +56,13 @@ mutable struct DataLoader
     start_stroke_token
 end
 
-function DataLoader(strokes; batchsize=100, dtype=Array{Array{Float32}},
-    max_seq_length=250,
+function DataLoader(strokes; batch_size=100,
+    max_seq_len=250,
     scale_factor=1.0,
     random_scale_factor=0.0,
     augment_stroke_prob=0.0,
     limit=1000, num_batches=0, start_stroke_token=[0, 0, 1, 0, 0])
-    DataLoader(strokes, batchsize, max_seq_length,
+    DataLoader(strokes, batch_size, max_seq_len,
     scale_factor,
     random_scale_factor,
     augment_stroke_prob,
@@ -38,14 +70,14 @@ function DataLoader(strokes; batchsize=100, dtype=Array{Array{Float32}},
 end
 
 function preprocess(d::DataLoader)
-    """Remove entries from strokes having > max_seq_length points."""
+    """Remove entries from strokes having > max_seq_len points."""
     strokes = deepcopy(d.strokes)
     raw_data = []
     seq_len = []
     count_data = 0
     for i in 1:size(strokes,1)
       data = strokes[i]
-      if size(data,1) <= (d.max_seq_length)
+      if size(data,1) <= (d.max_seq_len)
         count_data += 1
         # removes large gaps from the data
         data = min.(data, d.limit)
@@ -68,11 +100,29 @@ function preprocess(d::DataLoader)
     return d
 end
 
+function random_sample(d::DataLoader)
+  """Return a random sample, in stroke-3 format as used by draw_strokes."""
+  sample = deepcopy(rand(d.strokes))
+  return sample
+end
+
+function random_scale(d::DataLoader, data)
+  """Augment data by stretching x and y axis randomly [1-e, 1+e]."""
+  x_scale_factor = (
+      rand() - 0.5) * 2 * d.random_scale_factor + 1.0
+  y_scale_factor = (
+      rand() - 0.5) * 2 * d.random_scale_factor + 1.0
+  result = deepcopy(data)
+  result[:, 1] .*= x_scale_factor
+  result[:, 2] .*= y_scale_factor
+  return result
+end
+
 function calculate_normalizing_scale_factor(d::DataLoader)
   """Calculate the normalizing factor explained in appendix of sketch-rnn."""
   data = []
   for i in 1:size(d.strokes,1)
-    if size(d.strokes[i],1) > d.max_seq_length
+    if size(d.strokes[i],1) > d.max_seq_len
       continue
     end
     for j in 1:size(d.strokes[i],1)
@@ -81,4 +131,75 @@ function calculate_normalizing_scale_factor(d::DataLoader)
     end
   end
   return std(data)
+end
+
+function normalize(d::DataLoader, scale_factor=nothing)
+  """Normalize entire dataset (delta_x, delta_y) by the scaling factor."""
+  if !@isdefined scale_factor
+    scale_factor = calculate_normalizing_scale_factor(d)
+  d.scale_factor = scale_factor
+  end
+  for i in 1:size(d.strokes,1)
+    d.strokes[i][:, 1:2] ./= d.scale_factor
+  end
+  return d
+end
+
+function _get_batch_from_indices(d::DataLoader, indices)
+  """Given a list of indices, return the potentially augmented batch."""
+  x_batch = []
+  seq_len = []
+  #println("indices size: ", size(indices))
+  for idx in 1:size(indices,1)
+    i = indices[idx]
+    #println("i : ", i)
+    data = random_scale(d, d.strokes[i])
+    data_copy = deepcopy(data)
+    if d.augment_stroke_prob > 0
+      data_copy = augment_strokes(data_copy, d.augment_stroke_prob)
+    end
+    push!(x_batch,data_copy)
+    length = size(data_copy,1)
+    push!(seq_len,length)
+  seq_len = convert(Array{Int64}, seq_len)
+  end
+  #println("size x_batch: " ,size(x_batch))
+  # We return three things: stroke-3 format, stroke-5 format, list of seq_len.
+  return x_batch, pad_batch(d, x_batch, d.max_seq_len), seq_len
+end
+
+function random_batch(d::DataLoader)
+  """Return a randomised portion of the training data."""
+  idx = randperm(size(d.strokes,1))[1:d.batch_size]
+  return _get_batch_from_indices(d, idx)
+end
+
+function get_batch(d::DataLoader, idx)
+  @assert idx >= 0, "idx must be non negative"
+  @assert idx < d.num_batches, "idx must be less than the number of batches"
+  start_idx = idx * d.batch_size
+  indices = start_idx:(start_idx + d.batch_size-1)
+  return _get_batch_from_indices(d, indices)
+end
+
+function pad_batch(d::DataLoader, batch, max_len)
+  """Pad the batch to be stroke-5 bigger format as described in paper."""
+  result = zeros(Float32, d.batch_size, max_len + 1, 5)
+  @assert size(batch,1) == d.batch_size
+  for i in 1:d.batch_size
+    l = size(batch[i],1)
+    #println("size x_batch l: " ,size(batch[i]))
+    @assert l <= max_len
+    result[i, 1:l, 1:2] = batch[i][:, 1:2]
+    result[i, 1:l, 4] = batch[i][:, 3]
+    result[i, 1:l, 3] = 1 .- result[i, 1:l, 4]
+    result[i, l:end, 5] .= 1
+    # put in the first token, as described in sketch-rnn methodology
+    result[i, 2:end, :] = result[i, 1:end .!= end, :]
+    result[i, 1, :] .= 0
+    result[i, 1, 3] = d.start_stroke_token[3]  # setting S_0 from paper.
+    result[i, 1, 4] = d.start_stroke_token[4]
+    result[i, 1, 5] = d.start_stroke_token[5]
+  end
+  return result
 end
